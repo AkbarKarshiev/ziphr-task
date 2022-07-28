ba1a319db124b9534b2dd8965a993d32b4caa16f
AkbarKarshiev <50545784+AkbarKarshiev@users.noreply.github.com>
2022-07-28 15:16:51 +0500

    Initial commit

--- apps/assignment/src/app/app.component.ts
+++ apps/assignment/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ziphr-task-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'assignment';
